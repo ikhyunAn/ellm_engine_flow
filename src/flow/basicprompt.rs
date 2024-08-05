23efbcd2056f2753d9c029f8fc572c76b0c7012e
IkhyunAn <johnan0077@gmail.com>
2024-08-05 14:48:46 +0900

    basicprompt working with prompt template

--- src/flow/basicprompt.rs
+++ src/flow/basicprompt.rs
@@ -1,20 +1,20 @@
-use crate::{send_to_vllm, Data};
+use crate::{send_to_vllm, Data, PromptExchange};
 use super::{NewStateMachine, ProcessResult, StateMachine};
 
+use super::prompt_template::{BasicParams, BasicTemplate, PromptTemplate};
 
 /*
 NOTE: state machine may not be necessary for BasicPrompt */
 
-#[derive(Clone)]
-enum State {
-    Start,
-    Done,
-}
+// #[derive(Clone)]
+// enum State {
+//     Start,
+//     Done,
+// }
 
 #[derive(Clone)]
 pub struct BasicPromptStateMachine {
-    state: State,
-    _data: Option<Data>,
+    // state: State,
     // intermediate_results
     // output for client
 }
@@ -22,28 +22,44 @@ pub struct BasicPromptStateMachine {
 impl NewStateMachine for BasicPromptStateMachine {
     fn new() -> Self {
         BasicPromptStateMachine {
-            state: State::Start,
-            _data: None,
+            // state: State::Start,
         }
     }
 }
 
 impl StateMachine for BasicPromptStateMachine {
-    fn step(&mut self, data: Data) -> Result<ProcessResult, Box<dyn std::error::Error>> {
-        // DONE: send 1 request
-        match self.state {
-            State::Start => {
-                // TODO: prompting
-                send_to_vllm(data);
-                self.state = State::Done;
-                Ok(ProcessResult::Complete)
-            }
-            // FIXME: not necessary
-            State::Done => {
-                // respond_to_client(data);
-                Ok(ProcessResult::Complete)
-            }
-        }
+    fn step(&mut self, mut data: Data) -> Result<ProcessResult, Box<dyn std::error::Error>> {
+        let basic_template = BasicTemplate;
+        if let Some(fasoo) = &data.prompt_keys.fasoo {
+            
+            let basic_params = BasicParams {
+                task_instruction: fasoo.task_instruction.clone().unwrap(),
+                chunk: fasoo.chunks.clone().unwrap().get(0).unwrap().chunk_text.clone(),
+                user_query: data.prompt_keys.user_query.clone().unwrap(),
+            };
+
+            // reuse passed-in Data struct and create Option<PromptExchange> to Some
+            data.prompt_exchange = Some(PromptExchange{
+                index: 0,
+                prompted_string: basic_template.fill(&basic_params),
+                llm_response: None,
+            });
+            send_to_vllm(data);
+        };
+        Ok(ProcessResult::Complete)
+        
+        // self.state = State::Done;
+        // match self.state {
+        //     State::Start => {
+        //     }
+
+
+        //     // FIXME: not necessary
+        //     State::Done => {
+        //         // respond_to_client(data);
+        //         Ok(ProcessResult::Complete)
+        //     }
+        // }
     }
 }
 
